8685cc7889cdfd97e159e4be3b7547462d17e4cd
MichelleeLara <Mich.IA@hotmail.com>
2025-06-27 00:05:42 -0600

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '400px',
+      },
+    },
+  },
+  plugins: [],
+};
